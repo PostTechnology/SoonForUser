671f7d914ba6c8227972f0a4218ff1830c8cdbe2
Cherish-C <849756361@qq.com>
2018-05-01 19:06:22 +0800

    新增发现功能

---
+++ app/src/main/java/com/soon/android/adapterDataModels/Discover.java
@@ -0,0 +1,45 @@
+package com.soon.android.adapterDataModels;
+
+import java.util.List;
+
+public class Discover {
+    private String username;
+
+    private String comment;
+
+    private String time;
+
+    private List<Image> images;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
+}
